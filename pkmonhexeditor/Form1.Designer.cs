d762ac765d534134408cef40a2a2764c380cc00e
Marcelsuarez <captain.falcon.cool@gmail.com>
2021-06-09 13:59:19 -0500

    Add project files.

---
+++ pkmonhexeditor/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+
+using System;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+namespace pkmonhexeditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+        private Dictionary<string, string> ByteData;
+        private Dictionary<int, string> pkmonData;
+        private List<string> ByteList;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadrom = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loadrom
+            // 
+            this.loadrom.Location = new System.Drawing.Point(31, 52);
+            this.loadrom.Name = "loadrom";
+            this.loadrom.Size = new System.Drawing.Size(143, 37);
+            this.loadrom.TabIndex = 0;
+            this.loadrom.Text = "Load Rom";
+            this.loadrom.UseVisualStyleBackColor = true;
+            this.loadrom.Click += new System.EventHandler(this.loadrom_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "No rom detected";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(31, 229);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(226, 28);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Leave += new System.EventHandler(this.comboBox1_Leave);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Set Starter 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(278, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(226, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Set Starter 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.AllowDrop = true;
+            this.comboBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(278, 228);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(226, 28);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.Leave += new System.EventHandler(this.comboBox2_Leave);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.AllowDrop = true;
+            this.comboBox3.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox3.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(536, 228);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(221, 28);
+            this.comboBox3.TabIndex = 6;
+            this.comboBox3.Leave += new System.EventHandler(this.comboBox3_Leave);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(536, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(221, 29);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Set Starter 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(174, -140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 461);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loadrom);
+            this.Name = "Form1";
+            this.Text = "Starter Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadrom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private Button button2;
+        private ComboBox comboBox2;
+        private ComboBox comboBox3;
+        private Button button3;
+        private Label label2;
+    }
+}
+
---
+++ pkmonhexeditor/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace pkmonhexeditor
+{
+    public partial class Form1 : Form
+    {
+        protected enum Game
+        {
+            EMER,
+            SAPP,
+            RUBY,
+            NONE
+        }
+        private Dictionary<Game, int> offsets = new Dictionary<Game, int>{[Game.EMER] = 0x5B1DF8, [Game.RUBY] = 0x3F76E0, [Game.SAPP] = 0x3F771C};
+        //these offsets might vary by rom
+        private int currentoffset = 0;
+        private string tfilename = null;
+        private bool validloaded = false;
+
+        public Form1()
+        {
+            using (StreamReader r = new StreamReader("bytedata.json"))
+            {
+                string json = r.ReadToEnd();
+
+                this.ByteData = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+                this.ByteList = ByteData.Keys.ToArray().ToList();
+
+            }
+            
+            InitializeComponent();
+            //get another dict so we can read the bytes on the rom
+            this.pkmonData = this.ByteData.Keys.ToDictionary(x => Convert.ToInt32(ByteData[x], 16), x => x);
+            //set the list to all of them
+            this.comboBox1.DataSource = new List<string>(ByteList);
+            this.comboBox2.DataSource = new List<string>(ByteList);
+            this.comboBox3.DataSource = new List<string>(ByteList);
+
+
+        }
+        
+
+
+
+        private void loadrom_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+
+            DialogResult result = fdlg.ShowDialog(); 
+
+            if (result == DialogResult.OK) 
+            {
+                tfilename = fdlg.FileName;
+
+                label2.Text = tfilename;
+
+                Game game = checkFile(tfilename);
+
+                string loadedtext = "Not loaded";
+
+                switch (game)
+                {
+                    case Game.EMER:
+                        loadedtext = "Emerald loaded!";
+                        validloaded = true;
+                        currentoffset = offsets[game];
+                        break;
+                    case Game.RUBY:
+                        loadedtext = "Ruby loaded!";
+                        validloaded = true;
+                        currentoffset = offsets[game];
+                        break;
+                    case Game.SAPP:
+                        loadedtext = "Sapphire loaded!";
+                        validloaded = true;
+                        currentoffset = offsets[game];
+                        break;
+                    case Game.NONE:
+                        loadedtext = "File not RSE roms!";
+                        currentoffset = 0;
+                        validloaded = false;
+                        break;             
+                }
+
+                setDefault();
+
+                label1.Text = loadedtext;
+
+            }
+
+        }
+
+
+
+        private Game checkFile(string filename)
+        {
+            string check = "None";
+            
+            using (BinaryReader reader = new BinaryReader(File.Open(filename, FileMode.Open))) //opening file stream
+            {
+
+                byte[] store = new byte[4];
+                reader.BaseStream.Seek(0xA8, SeekOrigin.Begin);
+                reader.Read(store, 0x0, 0x4); //check if these bytes are EMER, RUBY or SAPP
+                check = System.Text.Encoding.Default.GetString(store);
+                reader.Dispose();
+
+                if (new List<string>{ "EMER", "RUBY", "SAPP"}.Contains(check))
+                {
+                    return (Game)Enum.Parse(typeof(Game), check);
+                }
+                else
+                {
+                    return Game.NONE;
+                }
+
+                
+                    
+            }
+           
+            
+        }
+
+        private void setDefault()
+        {
+            //set default to each of them by reading the rom
+            this.comboBox1.SelectedIndex = this.comboBox1.FindString(readStarter(0));
+            this.comboBox2.SelectedIndex = this.comboBox2.FindString(readStarter(2));
+            this.comboBox3.SelectedIndex = this.comboBox3.FindString(readStarter(4));
+        }
+
+
+        private void comboBox1_Leave(object sender, EventArgs e)
+        {
+            if (!comboBox1.Items.Contains(comboBox1.Text)) //making sure the text in the box is valid since you can autocomplete
+            {
+                MessageBox.Show("Not valid Pokemon name", "Invalid Pokemon name");
+                comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void comboBox2_Leave(object sender, EventArgs e)
+        {
+            if (!comboBox2.Items.Contains(comboBox2.Text)) 
+            {
+                MessageBox.Show("Not valid Pokemon name", "Invalid Pokemon name");
+                comboBox2.SelectedIndex = 0;
+            }
+        }
+
+        private void comboBox3_Leave(object sender, EventArgs e)
+        {
+            if (!comboBox3.Items.Contains(comboBox3.Text)) 
+            {
+                MessageBox.Show("Not valid Pokemon name", "Invalid Pokemon name");
+                comboBox3.SelectedIndex = 0;
+            }
+        }
+
+
+        private Tuple<byte, byte> parseHexval(string hex)
+        {
+            string first = hex.Substring(0, 4);
+
+            string second =  "0x" + hex.Substring(4, 2);
+
+            //tuples are fun
+
+            return new Tuple<byte, byte>(Convert.ToByte(first, 16), Convert.ToByte(second, 16));
+        }
+
+
+        private void writeStarter(int extraoffset, string order, string combo)
+        {
+            if (!validloaded)
+            {
+                MessageBox.Show("Please load in valid Pokemon R/S/E ROM", "Invalid ROM");
+            }
+            else
+            {
+                using (BinaryWriter writer = new BinaryWriter(File.Open(tfilename, FileMode.Open))) //opening file stream
+                {
+                    var newoffset = currentoffset + extraoffset;
+
+                    writer.BaseStream.Seek(newoffset, SeekOrigin.Begin);
+                    string hexval = ByteData[combo];
+
+                    Tuple<byte, byte> bytes = parseHexval(hexval);
+
+                    writer.Write(bytes.Item1);
+                    writer.Write(bytes.Item2);
+                    writer.Dispose();
+                    
+                    MessageBox.Show($"{combo} written as {order} starter with bytes {hexval}", "Success!");
+                }
+            }
+        }
+
+
+        private int BytesToInt(byte[] starter)
+        {
+            string[] str = new string[2];
+            str[0] = starter[0].ToString("X");
+            str[1] = starter[1].ToString("X");
+
+            for (int i = 0; i < 2; i++)
+            {
+                if (str[i].Length == 1)
+                {
+                    str[i] = "0" + str[i];
+                }
+            }
+            string hv = "0x" + str[0] + str[1];
+            int val = Convert.ToInt32(hv, 16);
+            return val;
+
+        }
+
+        private string readStarter(int extraoffset)
+        {
+            using (BinaryReader reader = new BinaryReader(File.Open(tfilename, FileMode.Open)))
+            {
+
+                reader.BaseStream.Seek(currentoffset + extraoffset, SeekOrigin.Begin);
+                byte[] starter = reader.ReadBytes(2);
+
+
+                int pkmonkey = BytesToInt(starter);
+                reader.Dispose();
+                //if the key is invalid then default to nothing
+                try
+                {
+                    return pkmonData[pkmonkey];
+                }
+                catch (KeyNotFoundException e)
+                {
+                    return "";
+                }
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            writeStarter(0, "first", comboBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            writeStarter(2, "second", comboBox2.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            writeStarter(4, "third", comboBox3.Text);
+        }
+
+
+
+    }
+}
